23be45e9e3dd4ff14ec190868051b907fe4a5189
Aleksandr Larochkin <infernalred13@gmail.com>
2020-08-25 20:15:12 +0300

    xsxs

---
+++ api/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Review, Comment
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "text", "author", "score", "pub_date")
+    search_fields = ("text",)
+    list_filter = ("pub_date", "title",)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "review", "text", "author", "pub_date")
+    search_fields = ("text",)
+    list_filter = ("pub_date", "author", "review",)
+    empty_value_display = '-пусто-1'
